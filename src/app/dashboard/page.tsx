b54464ff8ff94959b4c2607dfea24e71ecec033c
khalidch73 <khalidnawazch73@gmail.com>
2023-11-05 23:54:11 +0500

    aaaa

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,6 @@
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+import Link from 'next/link'
+
+export default function Page() {
+    return <Link href="/setting">Dashboard</Link> 
+  }
---
+++ src/app/dashboard/setting/password/page.tsx
@@ -0,0 +1,7 @@
+function Password() {
+    return(
+    <div>
+        Password
+    </div>
+    )
+}export default Password
